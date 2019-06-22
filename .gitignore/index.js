8cb6d2ec43515900fb08f363f4ff038b12ca3ae2
Okue92 <52104932+Okue92@users.noreply.github.com>
2019-06-22 15:56:02 +0200

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const low = require('lowdb');
+const FileSync = require ('lowdb/adapters/fileSync')
+const client = new Discord.Client();
+
+const adapter = new FileSync('database.json');
+const db = low(adapter);
+
+db.defaults({histoires: [], xp: []}).write() 
+var prefix = ("s!")
+
+bot.on('ready', function() {
+    bot.user.setGame("Okue Is Back");
+    console.log("Connectedç");
+});
+
+bot.login(NDg5MTAwNjc0NzkzOTk2Mjg4.DovtqQ.DjniNZZBXmSbws4dU_hDfnc92tw);
+
+
+bot.on('message', message => {
+    if(message.content.startsWith('alldm')){
+   let cont = message.content.slice(1).split(" ")
+           let args = cont.slice(1)
+           let member = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0])
+         message.guild.members.forEach((player) => {
+             message.guild.member(player).send(`Rejoins .
+   
+   
+    LOVE SOCIAL https://discord.gg/KSBxDWd`);
+   });
+       }
+   })
